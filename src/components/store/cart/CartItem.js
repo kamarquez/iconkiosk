de1669f0c9d47a05910619acdac22b4ee64edc87
Karina Marquez <karina@xyclon.com.ar>
2020-09-21 03:44:09 -0300

    desafio Rutas completo (fix match.params.id) + desafio evento sincronizacion counter

--- src/components/store/cart/CartItem.js
+++ src/components/store/cart/CartItem.js
@@ -1,20 +1,28 @@
 import React from "react";
 
 export default function CartItem(){
-    return(
+    let product= {
+        id: 1,
+        category: 2,
+        name: "Especieros Steel",
+        description: "Especieros con imán y base metálica. Cada especiero tiene doble apertura para especiar.",
+        price: 3790,
+        photo: "/images/1.jpg",
+    }
+        return(
         <tr>
         <td data-th="Product">
             <div className="row">
                 <div className="col-md-3 text-left">
-                    <img src="https://via.placeholder.com/250x250/5fa9f8/ffffff" alt="" className="img-fluid d-none d-md-block rounded mb-2 shadow " />
+                    <img src={product.photo} alt="" className="img-fluid d-none d-md-block rounded mb-2 shadow " />
                 </div>
                 <div className="col-md-9 text-left mt-sm-2">
-                    <h4>Product Name</h4>
-                    <p className="font-weight-light">Brand &amp; Name</p>
+                    <h4>{product.name}</h4>
+                    <p className="font-weight-light">{product.description}</p>
                 </div>
             </div>
         </td>
-        <td data-th="Price">$49.00</td>
+        <td data-th="Price">${product.price}</td>
         <td data-th="Quantity">
             <input type="number" className="form-control form-control-lg text-center" />
         </td>
